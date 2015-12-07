413479da9a1fbcf4e4491ca09141861906ea7249
Corey Osman <corey@logicminds.biz>
2015-12-07 11:53:11 -0800

    Backport PUP-5282 -  https://tickets.puppetlabs.com/browse/PUP-5282
      * this bug was fixed in puppet 4.3 but we are using pe 3.8
        so I have backported the fix to the pe-gem
      * previosuly this provider would only work on non-windows
        since windows installed the gem binary in the same place
        for FOSS and PE.  However, this changes the provider code
        to allow for windows support as well in order to work around
        the bug PUP-5282

--- lib/puppet/provider/package/pe_gem.rb
+++ lib/puppet/provider/package/pe_gem.rb
@@ -10,8 +10,11 @@ Puppet::Type.type(:package).provide :pe_gem, :parent => :gem do
     repositories."
 
   has_feature :versionable, :install_options
-
-  commands :gemcmd => "/opt/puppet/bin/gem"
+  if Facter.value(:kernel) == 'windows'
+    commands :gemcmd => "gem"
+  else
+    commands :gemcmd => "/opt/puppet/bin/gem"
+  end
 
   def self.instances
     if Puppet[:version].to_f >= 4.0
@@ -19,5 +22,45 @@ Puppet::Type.type(:package).provide :pe_gem, :parent => :gem do
     end
 
     super
+  end
+
+  def install(useversion = true)
+    command = [command(:gemcmd), "install"]
+    command << "-v" << resource[:ensure] if (! resource[:ensure].is_a? Symbol) and useversion
+
+    if source = resource[:source]
+      begin
+        uri = URI.parse(source)
+      rescue => detail
+        self.fail Puppet::Error, "Invalid source '#{uri}': #{detail}", detail
+      end
+
+      case uri.scheme
+        when nil
+          # no URI scheme => interpret the source as a local file
+          command << source
+        when /file/i
+          command << uri.path
+        when 'puppet'
+          # we don't support puppet:// URLs (yet)
+          raise Puppet::Error.new("puppet:// URLs are not supported as gem sources")
+        else
+          # check whether it's an absolute file path to help Windows out
+          if Puppet::Util.absolute_path?(source)
+            command << source
+          else
+            # interpret it as a gem repository
+            command << "--source" << "#{source}" << resource[:name]
+          end
+      end
+    else
+      command << "--no-rdoc" << "--no-ri" << resource[:name]
+    end
+
+    command += install_options if resource[:install_options]
+
+    output = execute(command)
+    # Apparently some stupid gem versions don't exit non-0 on failure
+    self.fail "Could not install: #{output.chomp}" if output.include?("ERROR")
   end
 end
